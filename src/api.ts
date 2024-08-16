103c842a3b7eb1a9c8744db13c850f2576286bbe
Emir Tüfekci <e.tufekci@simplydeliver.com>
2024-08-16 13:31:32 +0300

    Axios ContentType and credentials

--- src/api.ts
+++ src/api.ts
@@ -3,9 +3,9 @@ import axios from 'axios';
 const API_BASE_URL = 'http://localhost:3000';
 
 export const login = async (username: string, password: string) => {
-  return axios.post(`${API_BASE_URL}/auth/login`, { username, password });
+  return axios.post(`${API_BASE_URL}/auth/login`, { username, password },{headers: {'Content-Type': 'application/json'}, withCredentials : true});
 };
 
 export const register = async (username: string, emailaddress: string, password: string) => {
-  return axios.post(`${API_BASE_URL}/auth/register`, { username, emailaddress, password });
+  return axios.post(`${API_BASE_URL}/auth/register`, { username, emailaddress, password },{headers: {'Content-Type': 'application/json'}, withCredentials : true});
 };
